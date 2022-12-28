81ddba2881795d01d12d9bcdc3fab3b9b0539cf6
TA1028 <dina.wulan.ft21@mail.umy.ac.id>
2022-12-28 17:16:39 +0700

    Membuat  public Node Leftchild

--- BinarySearchTree_028/Program.cs
+++ BinarySearchTree_028/Program.cs
@@ -9,10 +9,25 @@ namespace BinarySearchTree_028
 
     class Node
     {
-        public string info; 
-        public Node Lefchild;
+        public string info;
+        public Node Leftchild;
         public Node rightchild;
+        //constructor for the Node Class
+
+        public Node(string i, Node l, Node r)
+        {
+            info = i;
+            Leftchild = l;
+            rightchild = r;
+        }
     }
+
+    /* A node class consists of three things, the information
+     * references to the right child, and references to the left child*/
+
+  
+
+        
     internal class Program
     {
         static void Main(string[] args)
