a83f1bda89bb40acb7bf2023aac8b0eee4716aac
TA1028 <dina.wulan.ft21@mail.umy.ac.id>
2022-12-28 17:58:35 +0700

    menambahkan case 3, 4, 5

--- BinarySearchTree_028/Program.cs
+++ BinarySearchTree_028/Program.cs
@@ -132,13 +132,31 @@ namespace BinarySearchTree_028
                         {
                             Console.Write("Enter a word: ");
                         }
+                        string word = Console.ReadLine();
+                        x.insert(word);
                         break;
                     case '2':
                         {
                             x.inorder(x.ROOT);
                         }
                         break;
-
+                    case '3':
+                        {
+                            x.preorder(x.ROOT);
+                        }
+                        break;
+                    case '4':
+                        {
+                            x.postorder(x.ROOT);
+                        }
+                        break;
+                    case '5':
+                        return;
+                    default:
+                        {
+                            Console.WriteLine("Invalid Option");
+                                break;
+                        }
                 }
             }
         }
